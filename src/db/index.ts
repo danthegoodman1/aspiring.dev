6ad69e72e7826e21de43d67961e2f1751528ad02
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-01-20 10:30:51 -0500

    move log

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,8 @@ import { Database } from "bun:sqlite"
 import { logger } from "src/logger"
 
 const dbFileName = process.env.DB_FILENAME ?? "sqlite.db"
+logger.debug(`Using db file "${dbFileName}"`)
 export const db = new Database(dbFileName, {
   create: true,
 })
-logger.debug(`Using db file "${dbFileName}"`)
 db.exec("PRAGMA journal_mode = WAL;")

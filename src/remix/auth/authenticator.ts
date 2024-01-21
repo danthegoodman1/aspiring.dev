34571a41a0fa9abe734b7d55b264d400f16ca26c
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-01-21 14:18:34 -0500

    2 week cookie

--- src/remix/auth/authenticator.ts
+++ src/remix/auth/authenticator.ts
@@ -16,7 +16,7 @@ export let sessionStorage = createCookieSessionStorage({
     httpOnly: true,
     secrets: [process.env.COOKIE_SECRET!],
     secure: process.env.NODE_ENV === "production", // enable this in prod only
-    maxAge: 3600 - 60, // 1 min grace period, their tokens are 3600 and no refresh
+    maxAge: 3600 * 24 * 14, // 2 weeks
   },
 })
 

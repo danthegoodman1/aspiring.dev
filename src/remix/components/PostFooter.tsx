3167566c64cf35f3f3dec30e55e57b9785311292
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-02-02 19:08:55 -0500

    better footer

--- src/remix/components/PostFooter.tsx
+++ src/remix/components/PostFooter.tsx
@@ -1,8 +1,12 @@
 export default function PostFooter() {
   return (
-    <div className="shadow-xl mt-10 border-[1px] border-neutral-100 p-4 sm:p-6 flex flex-col justify-between rounded-lg">
-      <h4 className="text-neutral-500">
-        Find me on Twitter{" "}
+    <div className="shadow-xl mt-10 border-[1px] border-neutral-100 p-4 sm:p-6 flex flex-col justify-between rounded-lg text-neutral-500 gap-2">
+      <p>
+        Share with your friends, foes, colleagues, or hackernews if you found
+        this post interesting!
+      </p>
+      <p className="text-neutral-500">
+        Discuss with me on Twitter/X{" "}
         <a
           target="_blank"
           href="https://twitter.com/Dan_The_Goodman"
@@ -10,7 +14,7 @@ export default function PostFooter() {
         >
           @Dan_The_Goodman
         </a>
-      </h4>
+      </p>
     </div>
   )
 }
